b2b942f4e3db36e39f73cbd42d9d1cb9d66ef9a5
radiciandrea <andrea.radici93@libero.it>
2022-06-20 18:17:51 +0200

    Integration time added

--- Logistic_growth.R
+++ Logistic_growth.R
@@ -4,13 +4,14 @@ N0 = 2 # initial population
 r = 0.02 #instantaneous growth rate
 K = 2 * 10^6 #carrying capacity
 T_max = 1000 # simulation time limit
+dt = 1 # integration time interval
 
 
 N <- c()
 N <- c(N0, N)
 
 for (i in 1:T_max) {
-  N = c(N, N[i] + r*N[i]*(1 - N[i]/K))
+  N = c(N, N[i] + N[i]*dt*(1 - N[i]/K))
 }
 
 plot(N)
